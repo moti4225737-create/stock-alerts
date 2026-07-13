a37732234d1505aa1ab31f5d0a80d9f505049582
moti4225737-create <moti4225737@gmail.com>
2026-07-13 23:28:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,41 +6,57 @@ TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 FINNHUB_API_KEY = os.environ["FINNHUB_API_KEY"]
 
-# Stock to test
-SYMBOL = "AAPL"
+# Watchlist
+SYMBOLS = [
+    "LQDA",
+    "KTOS",
+    "NOC",
+    "OABI",
+    "ONDS",
+    "ACTU",
+    "STNE",
+    "MU",
+    "INTC"
+]
 
-# Finnhub Quote API
-quote = requests.get(
-    f"https://finnhub.io/api/v1/quote?symbol={SYMBOL}&token={FINNHUB_API_KEY}"
-).json()
+message = "📈 Stock Sentinel\n\n"
 
-print(quote)
+for symbol in SYMBOLS:
+    try:
+        url = (
+            f"https://finnhub.io/api/v1/quote"
+            f"?symbol={symbol}&token={FINNHUB_API_KEY}"
+        )
 
-price = quote["c"]
-change = quote["d"]
-percent = quote["dp"]
+        quote = requests.get(url, timeout=15).json()
 
-message = f"""
-📈 Stock Sentinel
+        if "c" not in quote:
+            message += f"{symbol}: No data\n"
+            continue
 
-Symbol: {SYMBOL}
+        price = quote["c"]
+        change = quote["d"]
+        percent = quote["dp"]
 
-Price: ${price}
+        message += (
+            f"{symbol}\n"
+            f"Price: ${price}\n"
+            f"Change: {change}\n"
+            f"Percent: {percent}%\n\n"
+        )
 
-Change: {change}
+    except Exception as e:
+        message += f"{symbol}: ERROR ({e})\n\n"
 
-Percent: {percent}%
-"""
-
-# Telegram
-url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+telegram_url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
 requests.post(
-    url,
+    telegram_url,
     data={
         "chat_id": TELEGRAM_CHAT_ID,
         "text": message
-    }
+    },
+    timeout=15
 )
 
-print("Stock alert sent.")
+print("Message sent.")
